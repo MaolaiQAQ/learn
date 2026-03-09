2fc4e99f34b2ba5e36ffa22569f53f56dbfa4978
Maolai <maolaiqaq@outlook.com>
2026-03-09 12:25:59 +0800

    add helloworld

---
+++ Golang/02Variable/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var userName = "alex"
+
+func main() {
+	var name string
+	name = "golang"
+	fmt.Println(name)
+
+	var age int = 20
+	fmt.Println("age is", age)
+
+	sex := "boy"
+	fmt.Println("sex is", sex)
+
+	fmt.Println(userName)
+}
